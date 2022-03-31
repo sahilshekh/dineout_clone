36e9ba762daba76e1f85049c54455ed489f2bc53
Sahil Shekh <95908470+sahilshekh@users.noreply.github.com>
2022-03-31 13:36:14 +0530

    sahil

--- frontend/src/components/Booktable/Booktables.jsx
+++ frontend/src/components/Booktable/Booktables.jsx
@@ -25,37 +25,44 @@ function Booktable() {
         <div className='main_Booktable'>
           <div className='sideBar'>
               <form>
-                  <h3>Quick Filters</h3>
-                  <input type="search" placeholder='Search'></input><br/><br/>
+                  <h3 className='h3'>Quick Filters<p1 className="substract">-</p1></h3>
+                  <input type="search" placeholder='Search' className='serch'></input><br/><br/>
+                  <Link to="/Dineout"><input type="checkbox" className='checkbox'></input></Link><span className='clor'>Dineout pay</span><br/><br/>
+                  <Link to="/PureVeg"><input type="checkbox"className='checkbox'></input></Link><span className='clor'>Pure Veg</span><br/><br/>
+                 <Link to="/Star"><input type="checkbox"className='checkbox'></input></Link><span className='clor'>5 Star</span><br/><br/>
+                 <Link to="/Buffet"><input type="checkbox"className='checkbox'></input></Link><span className='clor'>Buffet</span><br/><br/>
 
-                  
-
-                  <Link to="/Dineout"><input type="checkbox"></input></Link>Dineout pay<br/><br/>
-                  <Link to="/PureVeg"><input type="checkbox"></input></Link>Pure Veg<br/><br/>
-                 <Link to="/Star"><input type="checkbox"></input></Link>5 Star<br/><br/>
-                 <Link to="/Buffet"><input type="checkbox"></input></Link>Buffet<br/><br/>
-
-                  <p>Show More (4)</p>
+                  <p className='more'>Show More (4)</p>
 
                   <hr/>
 
-                  <h3>Cuisines</h3>
-                  <input type="search" placeholder='Search For Cuisine'></input><br/><br/>
-                  <Link to="/NorthIndia"><p>North Indian</p></Link><br/><br/>
-                  <Link to="/Chinese">Chinese</Link><br/>
-                  <Link to="/Fast">Fast Food</Link><br/>
-                  <p>Continental</p>
-                  <p>Show More (60)</p>
+                  <h3 className='h3'>Cuisines <p1 className="substract2">-</p1></h3>
+                  <input type="search" placeholder='Search For Cuisine'className='serch'></input><br/><br/>
+                  <Link to="/NorthIndia"style={{ textDecoration: 'none',color:"gray",marginLeft:"15px" }}>North Indian</Link><br /><br />
+                  <Link to="/Chinese"style={{ textDecoration: 'none',color:"gray",marginLeft:"15px" }}><>Chinese</></Link><br /><br />
+                  <Link to="/Fast"style={{ textDecoration: 'none',color:"gray",marginLeft:"15px" }}><>Fast Food</></Link><br/>
+                  <p className='more'>Continental</p>
+                  <p className='more'>Show More (60)</p>
 
                   <hr/>
 
-                  <h3>Tags</h3>
+                  <h3 className='h3'>Tags<p1 className="substract3">-</p1></h3>
+                  <input type="search" placeholder='Search' className='serch'></input><br/><br/>
+                  <input type="checkbox"className='checkbox'></input><span><span className='clor'>Casual Dining</span></span><br /><br />
+                  <input type="checkbox"className='checkbox'></input><span><span className='clor'>Break Fast</span></span><br /><br />
+                  <input type="checkbox"className='checkbox'></input><span><span className='clor'>Best Pure Veg</span></span><br /><br />
+                  <input type="checkbox"className='checkbox'></input><span><span className='clor'>Pure veg</span></span><br /><br />
+                  <p className='more'>Show More (75)</p>
+               <hr />
+                 <h3 className='h3'>Features <p1 className="plus">+</p1></h3>
+                 <hr />
+                 <h3 className='h3'>Dineout Passport <p1 className="plus2">+</p1></h3>
+                 <hr />
+
               </form>
-
-
-
-
+              <p3 className="p2">Top 10 Restaurants in Mumbai</p3>
           </div>
+          
           <div className='dataDiv'>
               <div className="hotelData">
                 {products
