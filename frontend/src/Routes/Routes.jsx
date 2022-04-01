84661f335f18ff0b7278bf131d44c2d757787004
Asutosh Sahoo <asutosh.98.sahoo@gmail.com>
2022-04-01 11:05:33 +0530

    second

--- frontend/src/Routes/Routes.jsx
+++ frontend/src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router";
 import Booktable from "../components/Booktable/Booktables";
 import Home from "../components/Home/Home";
 import Hotel from "../components/SingleHotel/Hotel";
+import Dineoutpassport from '../components/Dineout-Passport/Dineoutpassport'
 
 
 export const AllRoutes = () => {
@@ -14,7 +15,7 @@ export const AllRoutes = () => {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/Booktable" element={<Booktable />} />
         <Route exact path="/Booktable/:name" element={<Hotel/>} />
-
+        <Route exact path="/Dineoutpassport" element={<Dineoutpassport/>} />
       </Routes>
     </>
   );
