d1b16f20a995cebace831f5ef344d3ad43377480
Sahil Shekh <95908470+sahilshekh@users.noreply.github.com>
2024-08-03 06:40:43 +0000

    api configration change

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -1,5 +1,5 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-module.exports=()=>{
-    return mongoose.connect("mongodb+srv://Dineout:Dineout$123@dineout.uy43k.mongodb.net/test")
+module.exports = () => {
+    return mongoose.connect("mongodb+srv://newDineout:Pass%40123@dineout.uy43k.mongodb.net/test");
 }
